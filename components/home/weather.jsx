ee6b20d5eeb85447107b31793c573c5759b0edcf
AlsolwiAisha <aisha.alsolwi@gmail.com>
2022-10-05 21:32:56 +0300

    Update weather.jsx

--- components/home/weather.jsx
+++ components/home/weather.jsx
@@ -1,34 +1,70 @@
-import { useState ,useEffect} from "react";
+import { useState } from "react";
 
-const Weather=(city)=>{
-    const[state,setStat]=useState([]);
-    // useEffect(() => {
-        async function getResponse() {
-
-            let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Sanaa&appid=52922fa07c551a030f9701eff376927a`);
-            if (!response.ok) {
-               throw new Error(`HTTP error! status: ${response.status}`);
-           }
-           setStat( await response.json());
-           console.log(state)
-           }
-           getResponse();
-         
+const Weather = ({ city, weather, onClick }) => {
+  const[city_,setCity_]=useState()
+    function CF(v1){
+        let x=(v1-32)/1.8;
+        return `${Math.round(x)}°C`
+    }
+    const handleKeyPress = (event) => {
+      if(event.key === 'Enter'){
+        onClick(city_)
+      }
+    }
+ 
+  
+    return (
+     <div className="all">
+       <div className="main_">
+       <div className="city">
+        <input className="city_IS" type="text" name="city_" id="city_"  placeholder='Enter city name' 
+          onChange={event => setCity_(event.target.value)}
+          onKeyPress={handleKeyPress}/>
+        <select className="city_IS" onChange={(e) => onClick(e.target.value)}>
+        {city.data.map((city, key) => {
+            return (
+              <option  key={key} >
+                {city.capital}
+              </option>
+            );
+          })}
+        </select>
+         <ul className="CitiesUl">
+          {city.data.map((city, key) => {
+            return (
+              <li  key={key} onClick={() => onClick(city.capital)}>
+                {city.capital}
+              </li>
+            );
+          })}
+        </ul>
+        </div>
+           <div className="cityWeather">
+           {weather.weather?<img width="150px" src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} />:null}
+           {weather.main?<div class="C_temp">{CF(weather.main.temp)}</div>:null}
+           {weather?<div>{weather.sys.country} / { weather.name}</div>:null}
+           {weather.weather? <div>{weather.weather[0].description}</div>:null}
+           </div>
+      </div>
+      <div className="bottom">
       
-// },[]);
+        
+        
+        <div class="minMx">
+        {weather.main? <div class="MX">     
+        <img width="50px" src={('/Images/high-temperature.png')} />
+                  {CF(weather.main.temp_max)}</div>:null}
+      {weather.main? <div class="MX"> <img width="50px" src={('/Images/temperature.png')} /> {CF(weather.main.temp_min)}</div>:null}
+        
+       </div>
 
+       {weather.main?  <div><img width="50px" src={('/Images/humidity.png')} />  {weather.main.humidity}%</div>:null}
+       {weather.wind?<div><img width="50px" src={('/Images/wind.png')} /> {weather.wind.speed} MPH</div>:null}
 
- return(<></>
-//      state.weather.map(item => {
-       
-//     return(
-       
-// <div>{item.weather.name}</div> 
-           
-//     );
-// }
-// )
-);
+      </div>
 
-};
-export default Weather; 
+      </div>
+    );
+  };
+  
+  export default Weather;
